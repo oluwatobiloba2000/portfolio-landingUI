9d590089ccbe8084eafc15ddcb54720a587a0168
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2020-05-26 21:01:21 +0100

    build component for a individual experience rendering

---
+++ src/components/IndividualResumeComponent.js
@@ -0,0 +1,82 @@
+import React from 'react';
+
+
+const IndividualResumeComponent = () =>(
+        <div className="resume-individual-container">
+         <div className="resume">
+             <ul>
+                 <li className="resume-top">
+                  <p className="resume-title">Teencode academy</p>
+                    <div className="resume-role">
+                        Product manager & Frontend developer
+                    </div>
+                 </li>
+                 <li className="resume-bottom">
+                    <p className="year">April 2020 - Aug 2020</p>
+                    <div className="resume-body">The first Intern I attended,</div>
+                 </li>
+             </ul>
+        </div>
+
+        <div className="resume">
+             <ul>
+                 <li className="resume-top">
+                  <p className="resume-title">Findworka academy</p>
+                    <div className="resume-role">
+                      Backend Intern
+                    </div>
+                 </li>
+                 <li className="resume-bottom">
+                <p className="year">Sep 2020 - Feb 2021</p>
+                <div className="resume-body">I applied as an Intern and got the job</div>
+                 </li>
+             </ul>
+        </div>
+
+        <div className="resume">
+             <ul>
+                 <li className="resume-top">
+                  <p className="resume-title">Naijahacks Hackathon</p>
+                    <div className="resume-role">
+                      Winner
+                    </div>
+                 </li>
+                 <li className="resume-bottom">
+                <p className="year">Dec 2019 - Jan 2020</p>
+                <div className="resume-body">Me and my team won Job creation category</div>
+                 </li>
+             </ul>
+        </div>
+        <div className="resume">
+             <ul>
+                 <li className="resume-top">
+                  <p className="resume-title">Naijahacks Hackathon</p>
+                    <div className="resume-role">
+                      Winner
+                    </div>
+                 </li>
+                 <li className="resume-bottom">
+                <p className="year">Dec 2019 - Jan 2020</p>
+                <div className="resume-body">Me and my team won Job creation category</div>
+                 </li>
+             </ul>
+        </div>
+        <div className="resume">
+             <ul>
+                 <li className="resume-top">
+                  <p className="resume-title">Naijahacks Hackathon</p>
+                    <div className="resume-role">
+                      Winner
+                    </div>
+                 </li>
+                 <li className="resume-bottom">
+                <p className="year">Dec 2019 - Jan 2020</p>
+                <div className="resume-body">Me and my team won Job creation category</div>
+                 </li>
+             </ul>
+        </div>
+        </div>
+
+)
+
+export default IndividualResumeComponent;
