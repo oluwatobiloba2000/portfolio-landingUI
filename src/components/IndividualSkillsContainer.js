8067cda21ba8a63e01c8fb31cc90bf1b03b1d2c1
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2020-05-26 21:03:13 +0100

    build component for a individual skills rendering in the resume page

---
+++ src/components/IndividualSkillsContainer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+
+const IndividualSkillsComponent = (props) =>(
+    <>
+      <a rel = "noopener noreferrer" target={"_blank"} href="https://dropbox.com" className="skills-box">
+        <img src="https://img.stackshare.io/service/146/thumb_retina_cbe630c29a62e283cef31afc5c4471da.png" className="skills-box-img" alt="Dropbox"/>
+        <div className="skills-box-text">Dropbox</div>
+      </a>
+      <a rel="noopener noreferrer" target={"_blank"} href="https://dropbox.com" className="skills-box">
+        <img src="https://img.stackshare.io/service/146/thumb_retina_cbe630c29a62e283cef31afc5c4471da.png" className="skills-box-img" alt="Dropbox"/>
+        <div className="skills-box-text">Dropbox</div>
+      </a>
+        <a rel="noopener noreferrer" target={"_blank"} href="https://firefox.com" className="skills-box">
+            <img src="https://img.stackshare.io/service/8705/thumb_retina_768px-Firefox_Logo__2017.svg.png" className="skills-box-img" alt="Firefox"/>
+            <div className="skills-box-text">Firefox</div>
+        </a>
+        <a rel="noopener noreferrer" target={"_blank"} href="https://js.com" className="skills-box">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg" className="skills-box-img" alt="Firefox"/>
+            <div className="skills-box-text">Javascript</div>
+        </a>
+    </>
+
+)
+
+export default IndividualSkillsComponent;
