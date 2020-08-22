5669edf0b8a85ff948bc9b9d9b30b81285fab88c
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2020-08-22 12:04:44 +0100

    corrected links for skills

--- src/components/IndividualSkillsContainer.js
+++ src/components/IndividualSkillsContainer.js
@@ -7,27 +7,27 @@ import expressimg from '../assets/images/express-logo.png';
 
 const IndividualSkillsComponent = () =>(
     <>
-      <a rel = "noopener noreferrer" target={"_blank"} href="https://dropbox.com" className="skills-box">
-        <img src={NodeImg} className="skills-box-img" alt="node"/>
+      <a rel = "noopener noreferrer" target={"_blank"} href="https://nodejs.org" className="skills-box">
+        <img src={NodeImg} className="skills-box-img" alt="nodejs"/>
         <div className="skills-box-text">Node</div>
       </a>
-      <a rel="noopener noreferrer" target={"_blank"} href="https://dropbox.com" className="skills-box">
+      <a rel="noopener noreferrer" target={"_blank"} href="https://reactjs.org" className="skills-box">
         <img src={reactimg} className="skills-box-img" alt="react"/>
         <div className="skills-box-text">React</div>
       </a>
-        <a rel="noopener noreferrer" target={"_blank"} href="https://firefox.com" className="skills-box">
+        <a rel="noopener noreferrer" target={"_blank"} href="https://redux.js.org" className="skills-box">
             <img src={reduximg} className="skills-box-img" alt="redux"/>
             <div className="skills-box-text">Redux</div>
         </a>
-        <a rel="noopener noreferrer" target={"_blank"} href="https://firefox.com" className="skills-box">
+        <a rel="noopener noreferrer" target={"_blank"} href="https://postgresql.org" className="skills-box">
             <img src={postgresimg} className="skills-box-img" alt="postgres"/>
             <div className="skills-box-text">POSTGRESQL</div>
         </a>
-        <a rel="noopener noreferrer" target={"_blank"} href="https://js.com" className="skills-box">
+        <a rel="noopener noreferrer" target={"_blank"} href="https://en.wikipedia.org/wiki/JavaScript" className="skills-box">
             <img src="https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg" className="skills-box-img" alt="Firefox"/>
             <div className="skills-box-text">Javascript</div>
         </a>
-        <a rel="noopener noreferrer" target={"_blank"} href="https://js.com" className="skills-box">
+        <a rel="noopener noreferrer" target={"_blank"} href="https://express.com" className="skills-box">
             <img src={expressimg} className="skills-box-img" alt="express"/>
             <div className="skills-box-text">Express</div>
         </a>
